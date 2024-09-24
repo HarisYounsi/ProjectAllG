0ab62574a231ab933a8ff302f640899f80365fb5
HarisYounsi <147699202+HarisYounsi@users.noreply.github.com>
2024-09-24 15:41:47 +0200

    Add files via upload

---
+++ Modele/paniermodele.php
@@ -0,0 +1,16 @@
+<?php
+require __DIR__ . "/../Libs/fonctionmodele.php";
+
+ 
+
+
+
+function delarticle($idarticle)
+{
+  $bdd = bdd();
+  $sql = "delete from panier where id_pan = ?";
+
+  $stmt= $bdd->prepare($sql);
+  $stmt->execute([$idarticle]);
+}
+
